1d9343fe7a43f0a8124db688b7a3f73367882afd
Hai <hugocerdajara@gmail.com>
2022-10-17 00:10:27 -0300

    avances test

--- crudtestBack/src/main/java/com/crudtest/crudtest/PersonaRepository.java
+++ crudtestBack/src/main/java/com/crudtest/crudtest/PersonaRepository.java
@@ -4,7 +4,6 @@ package com.crudtest.crudtest;
 import java.util.List;
 import org.springframework.data.repository.Repository;
 
-
 public interface PersonaRepository extends Repository<Persona, Integer> {
     List<Persona>findAll();
     Persona findById(int id);
--- crudtestBack/src/test/java/com/crudtest/crudtest/PersonaControllerTest.java
+++ crudtestBack/src/test/java/com/crudtest/crudtest/PersonaControllerTest.java
@@ -3,12 +3,43 @@ package com.crudtest.crudtest;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(PersonaController.class)
+@ComponentScan(basePackages = "com.crudtest.crudtest.*")
 public class PersonaControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+    
+    @MockBean
+    private PersonaService service;
+    
+    @MockBean
+    private PersonaRepository repository;
+    
     @Test
     void hello(){
         PersonaController controller = new PersonaController(); //arrange
         List<Persona> response = controller.listar(); //act
+    }
+    
+    @Test
+    public void testFindById() throws Exception {
+        mvc.perform(get("/personas/1")).andExpect(status().isOk());
     }
 }
